22dc2b56e1b14a3dd4645b780f4c4ce638c9c64a
Vaibhav Patel <vpatel1894@gmail.com>
2016-01-13 13:11:49 -0500

    Simple Http Request

--- main.go
+++ main.go
@@ -7,8 +7,28 @@ import (
 )
 
 func main() {
+
+/**
+  Sets up the web server
+  base url: "/" will have Hello World printed on the page
+**/
   http.HandleFunc("/",func (w http.ResponseWriter, r *http.Request) {
     fmt.Fprintln(w, "Hello World")
   })
+
+/**
+  making a simple get request to google
+  Make sure "net/http" is included
+**/
+  resp, err := http.Get("https://google.com")
+  if err != nil {
+    //handle error
+    fmt.Println("Error")
+  }else {
+    //print the response
+    fmt.Println(resp)
+  }
+
+  //running on 8080
   log.Fatal(http.ListenAndServe(":8080",nil))
 }
